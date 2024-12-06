d4d32c12a6791c98aedef06ee91daa2398172d72
Rony Shaji <ronyshaji1@gmail.com>
2024-12-06 16:30:34 +0100

    Changed the logic to find HH:MM:SS

--- Linux_System_Monitor/src/format.cpp
+++ Linux_System_Monitor/src/format.cpp
@@ -1,23 +1,29 @@
 #include <string>
+#include <sstream>
+#include <iomanip>
 
 #include "format.h"
+
 
 using std::string;
 
 string Format::ElapsedTime(long seconds) 
 { 
-    if (seconds > 0)
-    {
-        int hour = seconds / 3600;
-        double hourDouble = seconds / static_cast<double>(3600);
-        float minute = hourDouble - hour;
-        float minutes = minute * 60 ;
-        int min_int = static_cast<int>(minutes);
-        int second = (minutes - min_int) * 60;
-        return std::to_string(hour) + ":" + std::to_string(min_int) + ":" + std::to_string(second);
-    }
-    else 
-    {
-        return {"00:00:00"};
-    }
+   if (seconds > 0)
+   {
+       int hour = seconds / 3600;
+       int minute = (seconds % 3600) / 60;
+       int second = seconds % 60 ;
+
+       std::stringstream formatted_time;
+       formatted_time << std::setw(2) << std::setfill('0') << hour << ":"
+                      << std::setw(2) << std::setfill('0') << minute << ":"
+                      << std::setw(2) << std::setfill('0') << second;
+
+       return formatted_time.str();
+   }
+   else 
+   {
+       return "00:00:00";
+   }
 }
